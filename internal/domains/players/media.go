5ce2d9e6c751f80068785cb407432f2d3db5ce34
lhs <quintuplets2000@gmail.com>
2025-01-28 12:59:44 +0900

    fix: added db tag to entities

--- internal/domains/players/media.go
+++ internal/domains/players/media.go
@@ -8,14 +8,14 @@ import (
 
 // Media represents a media entity related to a player.
 type Media struct {
-	ID           uuid.UUID `json:"id"`
-	PlayerID     uuid.UUID `json:"player_id"` // Foreign key to Player
-	Source       string    `json:"source"`
-	URL          string    `json:"url"`
-	Title        string    `json:"title"`
-	Content      string    `json:"content"`
-	PublishedAt  time.Time `json:"published_at"`
-	ThumbnailURL string    `json:"thumbnail_url"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID           uuid.UUID `json:"id" db:"id"`
+	PlayerID     uuid.UUID `json:"player_id" db:"player_id"`
+	Source       string    `json:"source" db:"source"`
+	URL          string    `json:"url" db:"url"`
+	Title        string    `json:"title" db:"title"`
+	Content      string    `json:"content" db:"content"`
+	PublishedAt  time.Time `json:"published_at" db:"published_at"`
+	ThumbnailURL string    `json:"thumbnail_url" db:"thumbnail_url"`
+	CreatedAt    time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
 }
--- internal/domains/players/player_description.go
+++ internal/domains/players/player_description.go
@@ -8,9 +8,9 @@ import (
 
 // PlayerDescription represents a detailed description of a player.
 type PlayerDescription struct {
-	ID        uuid.UUID `json:"id"`
-	PlayerID  uuid.UUID `json:"player_id"` // Foreign key to Player
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uuid.UUID `json:"id" db:"id"`
+	PlayerID  uuid.UUID `json:"player_id" db:"player_id"`
+	Content   string    `json:"content" db:"content"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
